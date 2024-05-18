e121622caecf8e1f1c95823f20fa6dcbe2509704
Vanderley Junior <vanderley_1998@hotmail.com.br>
2024-05-18 11:47:06 -0300

    Initial Commit

---
+++ vj-forms/vj_formsApp.swift
@@ -0,0 +1,20 @@
+//
+//  vj_formsApp.swift
+//  vj-forms
+//
+//  Created by Vanderley Junior on 18/05/24.
+//
+
+import SwiftUI
+
+@main
+struct vj_formsApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
